df2f364a9ceed78caa00418eb036cbe5b84eb43b
agsuy <37564412+agsuy@users.noreply.github.com>
2019-09-24 00:12:08 -0300

    typo

--- stream.sh
+++ stream.sh
@@ -3,5 +3,5 @@
 set -e;
 
 /usr/bin/supervisord -c /home/novnc/supervisor/supervisord.conf > output.log 2>&1 &
-/bin/bash -c 'printf "\n\n\nCypress All in One\n\nURL: www.(hostIP):8080/vnc.html\nHint: yarn run cypress run --browser chrome\n\n"'
+/bin/bash -c 'printf "\n\n\nCypress All in One\n\nURL: www.(hostIP):8080/vnc.html\nHeadless test run: yarn run cypress run --browser chrome\nInteractive test run: yarn run cypress open\nRemember to copy your tests!\n\n"'
 /bin/bash
